8438bb2c6e20894f40cac3d79249db91bcadf439
Ajay Kumar Dhyani <ajaykumardhyani@gmail.com>
2020-09-28 19:58:48 +0530

    updated module

--- _example/example.tf
+++ _example/example.tf
@@ -3,10 +3,9 @@ provider "aws" {
 }
 
 module "lightsail" {
-  source                 = "../"
-  namespace              = "anmol"
-  environment            = "prod"
-  name                   = "clouddrove"
-  customer_email         = "abc@clouddrove.com"
-  customer_business_name = "Clouddrove Inc."
+  source      = "../"
+  environment = "prod"
+  name        = "lightsail"
+  application = "clouddrove"
+  label_order = ["environment", "application", "name"]
 }
