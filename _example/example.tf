66cfd2485ccb845ddd26a95e4bd990bb5c89c798
Ajay Kumar Dhyani <ajaykumardhyani@gmail.com>
2020-09-29 17:31:00 +0530

    update module

--- _example/example.tf
+++ _example/example.tf
@@ -2,18 +2,12 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-module "keypair" {
-  source          = "git::https://github.com/clouddrove/terraform-aws-keypair.git?ref=tags/0.12.3"
-  key             = "~/.ssh/id_rsa.pub"
-  key_name        = "devops"
-  enable_key_pair = true
-}
 
 module "lightsail" {
   source        = "../"
   environment   = "prod"
   name          = "lightsail"
-  key_pair_name = "devops"
+  key_pair_name = "testing"
   application   = "clouddrove"
   label_order   = ["environment", "application", "name"]
 }
